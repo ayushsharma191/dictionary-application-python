4b83b08f8a4412b8264df877347b28575ec9aab5
ayush <ayushsharma191@gmail.com>
2021-08-12 16:48:01 +0530

    Version 1.2 : Made few corrections in the code

--- dictionary-application-python/app-code.py
+++ dictionary-application-python/app-code.py
@@ -1,29 +1,32 @@
+#Version 1.2 of the Dictionary-App
 #importing the required libraries and modules
-
 import json
 from difflib import get_close_matches
 
 #opening the data.json file and loading it to a variable by the name data
-
 data = json.load(open("data.json"))
 
 #definition of the function which determines the response of the computer upon user input
-
 def translate(w):
 
-    #convert the word in all lower case
-
+    #convert the user input word in all lower case
     w = w.lower()
 
-    #condition 1 : if the word is found as it is as one of the key in data dictionary then return the value of that pair
-
+    #condition 1 : if the user input word is found as it is as one of the key in data dictionary then return the value of that pair
     if w in data:
         return data[w]
 
-    #Condition 2 : if the value entered is close enough to one of the existing keys in data dictionary then ask the user if they meant that key instead
+    #condition 2 : if the user input word sarts with a Captal letter in the dictionary, this shoulf be action.
+    elif w.title() in data:
+        return data[w.title()]
 
+    #condition 3 : if the user input word is in all capital letters in the dictionary then this shoulf be the action.
+    elif w.upper() in data:
+        return data[w.upper()]
+
+    #Condition 4 : if the value entered is close enough to one of the existing keys in data dictionary then ask the user if they meant that key instead
     elif len(get_close_matches(w, data.keys())) > 0:
-        yesno = input("Did you mean %s instead? Type 'y' for Yes or 'n' for No : " % get_close_matches(w, data.keys())[0])
+        yesno = input("Did you mean '%s' instead? Type 'y' for Yes or 'n' for No : " % get_close_matches(w, data.keys())[0])
         if yesno == "y" or yesno == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yesno == "n" or yesno == "No":
@@ -31,21 +34,17 @@ def translate(w):
         else:
             return "Wrong Input"
 
-    #Condition 3 : if there is no match then tell the user that the word is not in this dictionary
-
+    #Condition 5 : if there is no match then tell the user that the word is not in this dictionary
     else:
         return "Meaning of this word dosent exist in this dictionary !!"
 
 #take user input from user and store it in the variable word
-
 word = input("Enter a word to know its meaning : ")
 
 #pass the word argument in translate function and store its return value in variable
-
 output = translate(word)
 
 #print the variable in a good readable format for user 
-
 if type(output) == list:
     count = 1
     for item in output:
